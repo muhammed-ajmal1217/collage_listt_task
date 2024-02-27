2f597494718d8a90b9e8fb8cee3bd8ccddc5dcf7
muhammed-ajmal1217 <stranger99980@gmail.com>
2024-02-27 15:32:54 +0530

    completed

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -11,84 +11,104 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 39, 39, 39),
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 39, 39, 39),
-        foregroundColor: Colors.white,
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Color.fromARGB(255, 39, 39, 39),
+      //   foregroundColor: Colors.white,
+      // ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(left: 30,right: 30),
+          padding: const EdgeInsets.only(left: 20, right: 20),
           child: Container(
             height: 300,
             width: double.infinity,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              gradient: LinearGradient(
-                colors: [Colors.amber, Colors.orange], 
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+              gradient: LinearGradient( 
+                colors: [
+                  Color.fromARGB(255, 57, 57, 57),
+                  Color.fromARGB(255, 35, 35, 35)
+                ],
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
               ),
             ),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    data.name!,
-                    style: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        data.country!,
-                        style: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                      SizedBox(width: 10),
-                      Text('-'),
-                      SizedBox(width: 10),
-                      Text(
-                        data.alphaCode!,
-                        style: GoogleFonts.poppins(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.green),
-                      ),
-                    ],
-                  ),
-                  for (var dom in data.domain!) Text(dom),
-                  for (var link in data.webPage!)
-                    GestureDetector(
+            child: Column(
+              
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: InkWell(
                       onTap: () {
-                        loadUrl(link);
+                        Navigator.pop(context);
                       },
-                      child: Text(
-                        link,
-                        style: TextStyle(
-                          color: Colors.blue,
-                        ),
+                      child: Icon(Icons.arrow_back_ios,color: Colors.white,))),
+                ),
+                SizedBox(height: 30,),
+                Text(
+                  data.name!,
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.bold, fontSize: 20,color:Colors.white),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data.country!,
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold, fontSize: 20,color:Colors.white),
+                    ),
+                    SizedBox(width: 10),
+                    Text('-',style: TextStyle(color: Colors.white),),
+                    SizedBox(width: 10),
+                    Text(
+                      data.alphaCode!,
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.green),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10,),
+                for (var dom in data.domain!) Text(dom,style: TextStyle(color: Colors.white),),
+                SizedBox(height: 10,),
+                for (var link in data.webPage!)
+                  GestureDetector(
+                    onTap: () {
+                      loadUrl(link);
+                    },
+                    child: Text(
+                      link,
+                      style: TextStyle(
+                        color: Colors.blue,
                       ),
                     ),
-                  Text(data.stateProvince ?? 'State province is not available'),
-                ],
-              ),
+                  ),
+                  SizedBox(height: 10,),
+                Text(data.stateProvince ?? 'State province is not available',style: GoogleFonts.poppins(color:Colors.white),),
+                
+              ],
             ),
           ),
         ),
-
       ),
     );
   }
-loadUrl(String? url)async{
-  final link = Uri.parse(url!);
-  try {
-    if(await canLaunchUrl(link)){
-    launchUrl(link);
-  }else{
-    print('couldnot launch');
-  }
-  } catch (e) {
-    throw Exception("Error$e");
-  }
-}
 
+  loadUrl(String? url) async {
+    final link = Uri.parse(url!);
+    try {
+      if (await canLaunchUrl(link)) {
+        launchUrl(link);
+      } else {
+        print('couldnot launch');
+      }
+    } catch (e) {
+      throw Exception("Error$e");
+    }
+  }
 }
